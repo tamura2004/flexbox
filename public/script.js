4bcf842e0c69e22a4f5c52d41a31092af523d9a2
tamura <tamura2004@gmail.com>
2016-06-05 19:03:22 +0900

    Add public to gh-pages

---
+++ public/script.js
@@ -0,0 +1,17 @@
+$(function() {
+  return $("div").click(function() {
+    return $(this).animate({
+      top: "-10px"
+    }, 200).animate({
+      top: "-4px"
+    }, 200).animate({
+      top: "-7px"
+    }, 100).animate({
+      top: "-4px"
+    }, 100).animate({
+      top: "-6px"
+    }, 100).animate({
+      top: "-4px"
+    }, 100);
+  });
+});
